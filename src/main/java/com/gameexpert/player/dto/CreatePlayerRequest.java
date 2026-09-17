e404759b8d376a8537671d9abe328e4e767b6479
Thedume <jjojin7168@gmail.com>
2026-09-17 15:45:19 +0900

    닉네임 생성 규칙 설정

--- src/main/java/com/gameexpert/player/dto/CreatePlayerRequest.java
+++ src/main/java/com/gameexpert/player/dto/CreatePlayerRequest.java
@@ -1,11 +1,17 @@
 package com.gameexpert.player.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
 public class CreatePlayerRequest {
 
     // TODO Lv 3: 2~12글자의 영문 대소문자, 숫자와 밑줄을 허용하는 검증을 적용합니다.
+    @NotBlank(message = "닉네임은 비어 있을 수 없습니다.")
+    @Size(min = 2, max = 12, message = "닉네임은 2~12글자여야합니다.")
+    @Pattern(regexp="^[a-zA-Z0-9_]+$", message = "닉네임은 영문, 숫자, 밑줄만 사용할 수 있습니다.")
     private final String nickname;
 
     public CreatePlayerRequest(String nickname) {
