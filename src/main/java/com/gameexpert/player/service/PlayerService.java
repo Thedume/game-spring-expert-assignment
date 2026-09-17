42c5a5ede59b74a3d887be1fe5e35f0976dedc13
Thedume <jjojin7168@gmail.com>
2026-09-17 16:34:33 +0900

    플레이어 등록 기능 구현

--- src/main/java/com/gameexpert/player/service/PlayerService.java
+++ src/main/java/com/gameexpert/player/service/PlayerService.java
@@ -10,6 +10,7 @@ import com.gameexpert.player.dto.CreatePlayerRequest;
 import com.gameexpert.player.entity.Player;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Service
 @RequiredArgsConstructor
@@ -19,8 +20,10 @@ public class PlayerService {
 
     @Transactional
     public void createPlayer(CreatePlayerRequest request) {
-        // TODO Lv 3: 닉네임 중복을 확인하고 플레이어를 저장합니다.
-        throw new UnsupportedOperationException("Lv 3: 플레이어 등록을 구현하세요.");
+        if (playerRepository.existsByNickname(request.getNickname()))
+            throw new ConflictException("DUPLICATE_NICKNAME");
+
+        savePlayer(new Player(request.getNickname()));
     }
 
     private void savePlayer(Player player) {
